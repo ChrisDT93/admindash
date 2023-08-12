d4c7eef7c9569b1cd049b4bf84109b1c47dac386
Chris Turcios <christopher.d.turcios@gmail.com>
2023-08-11 17:58:51 -0700

    update migrations

--- src/main/java/com/admindash/Main.java
+++ src/main/java/com/admindash/Main.java
@@ -2,12 +2,15 @@ package com.admindash;
 
 import com.admindash.customer.Customer;
 import com.admindash.customer.CustomerRespository;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
+import java.util.Random;
 
 
 @SpringBootApplication
@@ -21,11 +24,18 @@ public class Main {
     CommandLineRunner runner(CustomerRespository customerRespository) {
 
         return args -> {
-            Customer alex = new Customer( "Alex", "alex@gmail.com", 20);
-            Customer jamila = new Customer( "Jamila", "jamila@gmail.com", 25);
+            var faker = new Faker();
+            Name name  = faker.name();
+            String firstName = name.firstName();
+            String lastName = name.lastName();
+            Random random = new Random();
 
-            List<Customer> customers = List.of(alex, jamila);
-//            customerRespository.saveAll(customers);
+            Customer customer = new Customer(
+                    firstName + " " + lastName,
+                    firstName.toLowerCase() + "." + lastName.toLowerCase() + "@gmail.com",
+                    random.nextInt(16, 99));
+
+            customerRespository.save(customer);
         };
     }
 
